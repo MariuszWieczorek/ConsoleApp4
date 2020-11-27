831575bc033185e424df2554a059f7866ff7f37f
Mariusz Wieczorek <m.wieczorek1972@gmail.com>
2020-11-27 08:59:41 +0100

    Add project files.

---
+++ Fisbus.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp4
+{
+    class Fisbus
+    {
+        public string ShowResult(int number)
+        {
+            if ((number % 3 == 0) && (number % 5 == 0))
+                return "FISBUS";
+            else if ((number % 3 == 0) && !(number % 5 == 0))
+                return "FIS";
+            else if (!(number % 3 == 0) && (number % 5 == 0))
+                return "BUS";
+            else
+                return number.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    // gra
+    // podzielna bez reszty przez 3 to FIS
+    // podzielna bez reszty przez 5 to BUS
+    // podzielna bez reszty przez 3 i 5 to FIZBUS
+    // nie podzielna anie przez 3 anie przez 5 to zwracamy liczbę
+    // metoda niestatyczna FIZBUS
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("FISBUS\nProszę podaj liczbę");
+            int number = GetNumber();
+            var myFisbus = new Fisbus();
+            string result = myFisbus.ShowResult(number);
+            Console.WriteLine(result);
+        }
+
+        static int GetNumber()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (!int.TryParse(input, out int number))
+                {
+                    string message = $"wpisanych znaków {input} nie można przekonwertować na liczbę, spróbuj ponownie";
+                    Console.WriteLine(message);
+                }
+                else
+                {
+                    return number;
+                }
+            }
+        }
+    }
+}
